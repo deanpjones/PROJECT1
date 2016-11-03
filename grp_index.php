67e869f65110cbfc23c3b56130a887e7c89f29bc
Ryan <ryan.eby@edu.sait.ca>
2016-11-03 12:21:25 -0600

    removed old files, updated index/style files

--- grp_index.php
+++ grp_index.php
@@ -2,63 +2,57 @@
 <!DOCTYPE html>
 <html>
 <head>
-
+<?php
+include("grp_header.php"); 
+?>
 <link rel="stylesheet" type="text/css" href="grp_style.css">
 
-<style>
-div {
-    width: 100px;
-    height: 40px;
-	//background: blue;
-    background-image: url("flyplane.png");
-	background-repeat: no-repeat;
-    
-    //background-attachment: fixed;
-	position: relative;
-    -webkit-animation: mymove 0s infinite; 
-    -webkit-animation-delay: 0s; /* Safari 4.0 - 8.0 */
-    animation: mymove 60s infinite;
-    animation-delay: 0s;
-}
 
-/* Safari 4.0 - 8.0 */
-@-webkit-keyframes mymove {
-    from {left: -10%;}
-    to {left: 110%;}
-}
-
-@keyframes mymove {
-    from {left: -10%;}
-    to {left: 110%;}
-}
-</style>
 </head>
 <body>
 
 
-<div></div>
-<p id='bigblue' >GROUP TRAVEL AGENCY</p>
-
-<br />
 
 
-<?php
+<div id='flyplane'></div><br/>
 
-$count=8;
-$rows=0;
 
-print("<table align='center' border=2 width=70%>");
-	
-	for($rows=0;$rows<$count; $rows++)
-	{
-	print("<tr>");
-		print("<td  width=50% align='center'>Sample Link</td> ");
-		print("<td width=50% align='center'>Sample  " . $rows . "</td>");
-	print("</tr>");
-	}
-print("</table>");
 
-?>
+
+
+<br/>
+<br/><br/><hr/><br/><br/>
+
+
+<p align='center'>
+<img width=50% height=35% src="flyplane.png" border=2></img>
+
+
+</p>
+
+
+
+<H1 align='center'>H1 Welcome Text Heading</H1>
+<p id='bodytext' >
+<table align='center' width=70%>
+<tr><td>
+This is where random text is randomly placed. This is where random text is randomly placed. This is where random text is randomly placed. This is where random text is randomly placed.
+Yes this is where random text is randomly placed. This is where random text is randomly placed. This is where random text is randomly placed. This is where random text is randomly placed. 
+This is where random text is randomly placed. This is where random text is randomly placed. This is where random text is randomly placed. This is where random text is randomly placed. 
+This is where random text is randomly placed. This is where random text is randomly placed. This is where random text is randomly placed. This is where random text is randomly placed. 
+</td></tr>
+
+<tr><td>
+<H2 align='center'>H2 Heading</H2>
+This is where random text is randomly placed. This is where random text is randomly placed. This is where random text is randomly placed. This is where random text is randomly placed. 
+This is where random text is randomly placed. This is where random text is randomly placed. This is where random text is randomly placed. 	
+</td></tr>
+</table>	
+
+</p>
+
+
+
 
 </body>
 </html>
